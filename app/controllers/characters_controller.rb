fb18b6b768ed104bfa5c1067b0aec87f6bba13e5
kinuno <kinu.hirata@gmail.com>
2021-05-16 11:56:59 +0900

    キャラクター一覧・詳細・編集・削除

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,12 +1,13 @@
 class CharactersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_room
+  before_action :set_character, only: [:show, :edit, :update, :destroy]
 
   def new
-    @room = Room.find(params[:room_id])
     @character = Character.new
   end
 
   def create
-    @room = Room.find(params[:room_id])
     @character = Character.new(character_params)
     if @character.save
       redirect_to room_path(@room)
@@ -15,7 +16,33 @@ class CharactersController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @character.update(character_params)
+      redirect_to room_character_path(@room.id, @character.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    redirect_to room_path(@room) if @character.destroy
+  end
+
   private
+
+  def set_room
+    @room = Room.find(params[:room_id])
+  end
+
+  def set_character
+    @character = Character.find(params[:id])
+  end
 
   def character_params
     params.require(:character).permit(:name, :personality).merge(user_id: current_user.id, room_id: params[:room_id])
