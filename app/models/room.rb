dc9818c2dccf3bec0d1f920e18cdcaec7077236e
kinuno <kinu.hirata@gmail.com>
2021-05-16 10:06:10 +0900

    characterのルーティング、コントローラー、モデル、DB作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :user
+  has_many :characters
 
   validates :name, presence: true, length: { maximum: 40 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
-  resources :rooms
- 
+  resources :rooms do
+    resources :characters
+  end
 end
