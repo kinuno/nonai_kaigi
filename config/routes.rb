1ca2e0e0a9d88a2badb36df7d4a2d7d24c285355
kinuno <kinu.hirata@gmail.com>
2021-06-07 18:41:28 +0900

    ルーティング整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
-  resources :rooms do
-    resources :characters
-    resources :comments
+  resources :rooms, only:[:index, :new, :create, :edit, :update, :destroy] do
+    resources :characters, only:[:new, :create, :show, :edit, :update, :destroy]
+    resources :comments, only:[:index, :create, :destroy]
   end
 end
